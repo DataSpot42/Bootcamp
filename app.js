678c89f09f27418f40e003e00a6e55f8fb77fb85
DataSpot42 <stevelookingforward@gmail.com>
2023-08-02 10:16:29 +0100

    updated keycodegen and titles

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 let hide = document.getElementById("boxes")
+let hide2 = document.getElementById("boxes2")
 let starttext = document.getElementById("starttext")    
         
 hide.style.display = "none"
+hide2.style.display = "none"
 starttext.style.display = "block"
 document.addEventListener("keypress",(event) => {
     
 const keyCodeCode = event.code
 const keyCodeWhich = event.which
+const keyCodeKey = event.key
 starttext.style.display = "none"
-hide.style.display = "flex"    
+hide.style.display = "flex"  
+hide2.style.display = "block"    
 document.getElementById("keycodeA").innerHTML = keyCodeCode;
 document.getElementById("keycodeB").innerHTML = keyCodeWhich;
+document.getElementById("keycodeC").innerHTML = keyCodeKey;
 })

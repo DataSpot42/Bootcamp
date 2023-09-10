a3e3d92a57e0c8e65d3710c6babfd079b03915f8
DataSpot42 <stevelookingforward@gmail.com>
2023-09-10 21:40:22 +0100

    Updated portfolio

---
+++ WelcomeBack.js
@@ -0,0 +1,29 @@
+
+
+
+class WelcomeBack extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            name: '',
+            apppVersion: ''
+        }
+    }
+render(){
+    return(
+        <>
+        <h2>Hello World</h2>
+        <button>Download</button>
+        </>
+    )
+}
+
+}
+
+//function ReactElement() {
+  // TODO: Actually implement a react Elelement
+ // return <h1>Hello from React!</h1>;
+//}
+
+//const domContainer = document.querySelector('#reactElement');
+//ReactDOM.render(e(ReactElement), domContainer);
